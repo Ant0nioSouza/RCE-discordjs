3cb95ef47af72ca1ddfe9b86e81a96f4160708e5
Antonio Souza <antoniosouza.dev@gmail.com>
2021-11-27 19:56:41 -0300

    chore: call initialize function

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import { Client, Intents } from "discord.js";
 
 const { token } = require("./config.json");
-const client: Client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client: Client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+initialize()
 
 function initialize(): void {
+    console.log("Connecting...")
     client.login(token);
 }
+
+client.on("ready", (): void => {
+    console.log("Bot online!")
+});
